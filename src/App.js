3897c22c71e3762cada33cba2852aa49a834c821
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-23 15:10:28 +0600

    added show dynamic data by Todo function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+    <Todos></Todos>
      <Person name={"Kazi Nazrul Islam"} nickname={"Dukhu Mia"} life={"1899-1976"} famousBook ={"Ognibina"} famousPoem={"Bidrohi"} honour={"National poet of Bangladesh"}></Person>
     </div>
   );
+}
+
+const TodoStyle ={
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '10px',
+  margin: '20px',
+ 
+}
+
+const gridStyle ={
+  backgroundColor: 'lightPink',
+  borderRadius: '10px',
+  boxShadow: '2px 2px 2px gray',
+  padding: '10px',
+  
+}
+
+function Todos (){
+  const [Todos, setTodos] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data =>{setTodos(data)
+          console.log(data)
+    })
+  },[])
+
+  return(
+    <div>
+      <h1> Welcome to Todos Website</h1>
+      <h5>Total Todos list: {Todos.length}</h5>
+     <div style={TodoStyle}>
+        {
+           Todos.map(todo =><Todo title ={todo.title} completed ={todo.completed} id={todo.id} userId={todo.userId} ></Todo>)
+         }
+     </div>
+    </div>
+  )
+
+}
+
+function Todo(props){
+  return(
+   
+     <div style={gridStyle}>
+      <h4>Title: {props.title}</h4>
+        <p>Completed: {props.completed}</p>
+        <p>UserId: {props.userId}, ID: {props.id}</p>
+     </div>
+    
+  )
+}
+
+
+
+const personStyle ={
+  backgroundColor:'lightGreen',
+  border: '2px solid red',
+  margin: '24px',
+  padding:'20px',
 }
 
 function Person(props){
@@ -18,7 +82,7 @@ function Person(props){
   }
   
   return(
-    <div>
+    <div style={personStyle}>
       <h2>Name: {props.name} </h2>
       <h4>Nickname: {props.nickname}</h4>
       <p>Lifetime Age: {props.life}</p>
